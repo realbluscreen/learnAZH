5a07c358a143669d1b68ffc599476ae98d540975
realbluscreen <82208404+realbluscreen@users.noreply.github.com>
2021-04-23 09:19:55 +0800

    Update PSConfigAD.ps1

--- PSConfigAD.ps1
+++ PSConfigAD.ps1
@@ -14,3 +14,5 @@ Format-Volume -FileSystem NTFS  -Confirm:$false
  Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
 
  Install-ADDSForest -DomainName "contoso.com" -InstallDns -DatabasePath "f:\NTDS" -SysvolPath "F:\SYSVOL" -LogPath "G:\LOGS"
+ 
+  Add-Computer -DomainName "contoso.com" -Credential (New-Object System.Management.Automation.PSCredential ("user1", (ConvertTo-SecureString 'Password' -AsPlainText -Force) ))
